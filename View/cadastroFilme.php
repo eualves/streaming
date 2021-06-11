0cfa32ebbbc407d65feab89f5bdec927a7752608
Leonardo <leoalvesbico@gmail.com>
2021-06-11 17:58:44 -0300

    Front end funcionario

--- View/cadastroFilme.php
+++ View/cadastroFilme.php
@@ -3,35 +3,37 @@
 include("header.php");
 ?>
 <div class="container">
-<form class="row g-3">
-    <div class="col-md-6">
-        <label for="inputFilme" class="form-label">Filme</label>
-        <input type="text" class="form-control" id="inputFilme">
-    </div>
-    <div class="col-md-6">
-        <label for="inputGenero" class="form-label">Gênero</label>
-        <input type="text" class="form-control" id="inputGenero">
-    </div>
-    <div class="col-12">
-        <label for="inputSinopse" class="form-label">Sinopse</label>
-        <input type="text" class="form-control" id="inputSinopse">
-    </div>
-    <div class="col-12">
-        <label for="inputAno" class="form-label">Ano</label>
-        <input type="text" class="form-control" id="inputAno"->
-    </div>
-    <div class="col-md-6">
-        <label for="inputCapa" class="form-label">Capa</label>
-        <input type="text" class="form-control" id="inputCapa">
-    </div>
-    <div class="col-md-4">
-        <label for="inputURL" class="form-label">URL</label>
-        <input type="text" class="form-control" id="inputURL">
-    </div>
-    <div class="col-12">
-        <button type="submit" class="btn btn-primary">Sign in</button>
-    </div>
-</form>
+    <form method="../Controller/inserirFilme.php" action="get" class="row g-3">
+        <div class="col-md-8">
+            <label for="inputFilme" class="form-label">Filme</label>
+            <input type="text" name="filme" class="form-control" id="inputFilme">
+        </div>
+        <div class="col-md-4">
+            <label for="inputGenero" class="form-label">Gênero</label>
+            <input type="text" name="genero" class="form-control" id="inputGenero">
+        </div>
+        <div class="col-2">
+            <label for="inputAno" class="form-label">Ano</label>
+            <input type="text" name="ano" class="form-control" id="inputAno" placeholder="2000">
+        </div>
+        <div class="col-md-10">
+            <label for="inputCapa" class="form-label">Capa</label>
+            <input type="text" name="capa" class="form-control" id="inputCapa">
+        </div>
+        <div class="col-md-12">
+            <label for="inputURL" class="form-label">URL</label>
+            <input type="text" name="url" class="form-control" id="inputURL">
+        </div>
+        <div class="form-floating">
+            <textarea class="form-control" name="sinopse " placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+            <label for="floatingTextarea2">Sinopse</label>
+        </div>
+
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Cadastro</button>
+        </div>
+    </form>
 </div>
 
 <?php
--- View/index.php
+++ View/index.php
@@ -2,33 +2,24 @@
 <html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Streaming</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Streaming</title>
 </head>
 
 <body>
-    <div class="container">
+  <div class="container">
 
-    <!--<ul class="nav justify-content-end">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>-->
-        <img class="fundo" src="img/imgfundo.png" alt="" >
-    </div>
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Logar</a>
+
+    </ul>
+    <img class="fundo" src="img/principalstreaming.gif">
+  </div>
 </body>
 
 </html>
